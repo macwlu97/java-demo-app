692b21c0e7f9339b1a0ea1b6ae3761a8f022230f
Maciej Wnuk Lipiński <maciej.wnuklipinski@gmail.com>
2019-03-12 11:55:13 +0100

    apka

---
+++ src/main/java/pl/wnuk/demoapp/DemoappApplication.java
@@ -0,0 +1,14 @@
+package pl.wnuk.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/wnuk/demoapp/HelloWorldEndpoint.java
@@ -0,0 +1,18 @@
+package pl.wnuk.demoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldEndpoint {
+
+        // localhost:8080:/hello
+        // Hello World!
+
+        @RequestMapping(method = RequestMethod.GET, path = "/hello")
+        String hello() {
+            return "Hello World!";
+        }
+    }
+
