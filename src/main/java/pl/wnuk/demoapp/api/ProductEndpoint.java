a952969119c870aa7cc74e485b8a06f1852ef085
Maciej Wnuk Lipiński <maciej.wnuklipinski@gmail.com>
2019-03-23 19:05:07 +0100

    fixed errors ProductRequestDto

--- src/main/java/pl/wnuk/demoapp/api/ProductEndpoint.java
+++ src/main/java/pl/wnuk/demoapp/api/ProductEndpoint.java
@@ -1,6 +1,7 @@
 package pl.wnuk.demoapp.api;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wnuk.demoapp.domain.ProductFacade;
@@ -11,16 +12,15 @@ import pl.wnuk.demoapp.domain.ProductResponseDto;
 @RequestMapping("/products")
 class ProductEndpoint {
 
+    private final ProductFacade productFacade;
+
     public ProductEndpoint(ProductFacade productFacade) {
         this.productFacade = productFacade;
     }
 
-    private final ProductFacade productFacade;
-
-
 
     @PostMapping
-    ProductResponseDto createProduct(ProductRequestDto productRequestDto){
+    ProductResponseDto createProduct(@RequestBody ProductRequestDto productRequestDto){
         return productFacade.create(productRequestDto);
     }
 }
--- src/main/java/pl/wnuk/demoapp/domain/ProductFacadeImpl.java
+++ src/main/java/pl/wnuk/demoapp/domain/ProductFacadeImpl.java
@@ -16,17 +16,20 @@ public class ProductFacadeImpl implements ProductFacade {
 
     @Override
     public ProductResponseDto create(ProductRequestDto productRequest) {
+        //walidacja
         if (!productRequest.isValid()){
             throw new RuntimeException("Product name cannot be empty!");
         }
 
+        //towrzenie
         String id = UUID.randomUUID().toString();
         LocalDateTime createdAt = LocalDateTime.now();
         Product product = new Product(id, productRequest.getName(), createdAt);
 
+        //zapis
         productRepository.save(product);
 
-        ProductResponseDto respone = new ProductResponseDto();
+//        ProductResponseDto respone = new ProductResponseDto();
         //stworzyć produkt
         //zapisać go gdzieś
         //przemapować Product -> ProductResponseDto i zwrócić
